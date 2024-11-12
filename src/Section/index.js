693b466acbeb369380c2df44f78483f628d213c0
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2024-11-12 18:15:59 +0000

    Section formatted

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,15 +1,13 @@
 import "./style.css";
 
-const Section = ({title, body, extraHeaderContent}) => (
+const Section = ({ title, body, extraHeaderContent }) => (
   <section className="section">
-        <div className="section__headerDiv">
-          <h2 className="section__header">{title}</h2>
-          {extraHeaderContent}
-        </div>
-        <div className="section__div">
-          {body}
-        </div>
-      </section>
+    <div className="section__headerDiv">
+      <h2 className="section__header">{title}</h2>
+      {extraHeaderContent}
+    </div>
+    <div className="section__div">{body}</div>
+  </section>
 );
 
 export default Section;
