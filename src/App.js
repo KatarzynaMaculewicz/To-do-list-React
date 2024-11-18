f99d9dfed8a3dac443aa211f7e9410c8815de6d8
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2024-11-18 13:22:15 +0000

    check is the new task field empty

--- src/App.js
+++ src/App.js
@@ -34,21 +34,27 @@ function App() {
   };
 
   const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task,
-      done: true,
-  })));
+    setTasks(tasks =>
+      tasks.map(task => ({
+        ...task,
+        done: true,
+      }))
+    );
   };
 
   const addNewTask = (content) => {
-    setTasks(tasks => [
-    ...tasks,
-    {
-      content,
-      done: false,
-      id: tasks.length ? tasks[tasks.length -1].id + 1 : 1,
-    },
-    ]);
+    if (content.length > 0) {
+      setTasks((tasks) => [
+        ...tasks,
+        {
+          content,
+          done: false,
+          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+        },
+      ]);
+    }
+
+    return;
   };
 
   return (
