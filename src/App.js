86ca3090995638d530b20a4b55f9cbbd697821c2
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2024-11-26 20:45:54 +0000

    localStorage added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Tasks from "./Tasks";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const defaultTasks = [
+  { id: 1, content: "przejść na Reacta", done: false },
+  { id: 2, content: "zjeść kolację", done: true },
+];
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
+ 
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || defaultTasks
+  );
 
-  const defaultTasks = [
-    { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zjeść kolację", done: true },
-];
-
-  const [tasks, setTasks] = useState(defaultTasks);
+  useEffect(() => localStorage.setItem("tasks", JSON.stringify(tasks)));
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone)
