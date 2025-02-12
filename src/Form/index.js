ec7ee27d1ec15c3b01a243455eab0566651bd90e
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-02-12 14:36:42 +0000

    focus on input

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Button, Input, StyledForm } from "./styled";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
 
+  const inputRef = useRef(null);
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
   const onFormSubmit = (event) => {
     event.preventDefault();
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
+    focusInput();
   };
 
   return (
     <StyledForm onSubmit={onFormSubmit}>
       <Input
+        ref={inputRef}
         value={newTaskContent}
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)}
