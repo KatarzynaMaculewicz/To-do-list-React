9018166e9a34eb83ad3d7230fc13bba2470fdac7
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-02-12 13:35:33 +0000

    GlobalStyle.js added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+  box-sizing: border-box;
+  }
+
+*,
+  ::after,
+  ::before {
+  box-sizing: inherit;
+  }
+
+  body {
+  font-family: 'Montserrat', Helvetica, sans-serif;
+  background: #EEE;
+  word-break: break-word;
+  }
+`;
