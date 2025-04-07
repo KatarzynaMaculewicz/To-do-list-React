d5a3bf5a8d641f2c6d92311eeb1db053113eb6b5
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-04-07 20:24:31 +0100

    GlobalStyle formatted

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,18 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   html {
-  box-sizing: border-box;
+    box-sizing: border-box;
   }
-
-*,
+  *,
   ::after,
   ::before {
-  box-sizing: inherit;
+    box-sizing: inherit;
   }
 
   body {
-  font-family: 'Montserrat', Helvetica, sans-serif;
-  background: #EEE;
-  word-break: break-word;
+    font-family: 'Montserrat', Helvetica, sans-serif;
+    background: #EEE;
+    word-break: break-word;
   }
 `;
