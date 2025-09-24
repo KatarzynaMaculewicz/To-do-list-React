445ff060cbbcdf6952dd3f4f6a9b3b3a4cb52434
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2025-09-24 10:52:53 +0100

    useTasks deleted

--- src/useTasks.js
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from "react";
-
-const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || defaultTasks
-  );
-
-  useEffect(() => localStorage.setItem("tasks", JSON.stringify(tasks)));
